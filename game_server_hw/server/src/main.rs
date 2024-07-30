026205e947fd4d3b801f6ae550edb207f503576a
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-07-30 15:42:07 +0900

    폴더 이름 변경

--- game_server_hw/server/src/main.rs
+++ game_server_hw/server/src/main.rs
@@ -12,13 +12,5 @@ async fn main() {
         }
     };
 
-    // let ip = "127.0.0.1";
-    // let port = 8080;
-
     server::tcp_server::run_server(&ip, port).await;
-
-    // let tcp_server_handle = tokio::spawn(server::tcp_server::run_server(ip, 8080));
-    // let udp_server_handle = tokio::spawn(server::udp_server::run_server(ip, 7878));
-
-    // let (_, _) = tokio::join!(tcp_server_handle, udp_server_handle);
 }
--- game_server_hw/server/src/server/mod.rs
+++ game_server_hw/server/src/server/mod.rs
@@ -1,4 +1,3 @@
 pub mod tcp_server;
-pub mod udp_server;
 pub mod world;
 pub mod client;
